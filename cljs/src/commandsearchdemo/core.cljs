6cf49b3c2a636f1d4614b9398fb7a6c8066d89d2
S H <you@example.com>
2018-12-07 23:51:10 -0600

    searching the list with the input

--- cljs/src/commandsearchdemo/core.cljs
+++ cljs/src/commandsearchdemo/core.cljs
@@ -3,9 +3,39 @@
 
 (enable-console-print!)
 
+(def query (r/atom ""))
+(def results (r/atom []))
+
+(defn fetch [url]
+  (-> (js/fetch url)
+      (.then #(.json %))))
+
+(defn update-results []
+  ; TODO: sanitize this
+  ; TODO:
+  (-> (str "/search/" @query)
+      (fetch)
+      (.then #(reset! results %))))
+
+(defn build-quake [data]
+  [:p { :key (.-_id data) }
+    (.-eq_primary data)
+    " "
+    (.-location_name data)
+    " "
+    " "
+    (.-date data)
+    ])
+
 (defn app []
     [:div [:p "Search Earthquakes"]
-          [:input]])
+          [:input { :value @query
+                    :on-change #(do (reset! query  (-> % .-target .-value))
+                                    (update-results))}]
+          (map build-quake @results)])
 
 (defn ^:export main []
     (r/render [app] (js/document.getElementById "app")))
+
+
+; TODO: add disclaimer about data and dates being defaulted and all
