3953af226b0e19cee104a4926efccd0ec2deeddf
ash <ash@ashs-MacBook-Pro.local>
2023-09-24 00:39:27 -0700

    clean

--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -1,24 +1,5 @@
-// To see this message, add the following to the `<head>` section in your
-// views/layouts/application.html.erb
-//
-//    <%= vite_client_tag %>
-//    <%= vite_javascript_tag 'application' %>
-console.log('Vite ⚡️ Rails')
-
-// If using a TypeScript entrypoint file:
-//
-// If you want to use .jsx or .tsx, add the extension:
-//     <%= vite_javascript_tag 'application.jsx' %>
-
-console.log('Visit the guide for more information: ', 'https://vite-ruby.netlify.app/guide/rails')
-
-// Example: Load Rails libraries in Vite.
-
-// Example: Import a stylesheet in app/frontend/index.css
-// import '~/index.css'
-
 import App  from '../App.svelte'
 
 const app = new App({
-    target: document.body
+  target: document.body
 })
