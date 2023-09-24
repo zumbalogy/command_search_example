7daa563e1c01b1e6039d9f1710d9ccb6de3970ea
ash <ash@ashs-MacBook-Pro.local>
2023-09-24 01:00:13 -0700

    clean

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@ Rails.application.routes.draw do
   get '/', to: 'home#splash'
   get '/search/', to: 'home#search'
   get '/search/:query', to: 'home#search'
-  get '/search/@/:query', to: 'home#search'
 end
