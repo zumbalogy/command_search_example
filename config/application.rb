f33e4782cdfcfb6cb2aef5f9c3a8c5a0a2f60d4d
ash <ash@ashs-MacBook-Pro.local>
2023-09-24 00:49:21 -0700

    clean

--- config/application.rb
+++ config/application.rb
@@ -2,17 +2,10 @@ require_relative "boot"
 
 require "rails"
 
-%w(
-  action_controller/railtie
-  action_view/railtie
-  action_text/engine
-  rails/test_unit/railtie
-).each do |railtie|
-  begin
-    require railtie
-  rescue LoadError
-  end
-end
+require "action_controller/railtie"
+require "action_view/railtie"
+require "action_text/engine"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
