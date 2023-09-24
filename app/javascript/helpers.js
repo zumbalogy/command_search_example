aaef2aedcd65a740d9e01e55fbd592f7f0cd3711
ash <ash@ashs-MacBook-Pro.local>
2023-09-24 00:46:30 -0700

    clean

--- app/javascript/helpers.js
+++ app/javascript/helpers.js
@@ -1,43 +1,46 @@
 export const fastQuakeFilter = (ids, list) => {
-  var output = [];
-  var list_index = 0;
+  const output = []
+  let list_index = 0
   ids.forEach((id, i) => {
     while (id !== list[list_index].id) {
-      list_index += 1;
+      list_index += 1
     }
     list[list_index].scrollIdx = i
     output.push(list[list_index])
-    list_index += 1;
+    list_index += 1
   })
-  return output;
+  return output
 }
 
 export const longLatFinder = (list) => {
-  var ttt = Date.now()
-  var minLat = 90;
-  var maxLat = -90;
-  var minLong = 180;
-  var maxLong = -180;
+  const ttt = Date.now()
+  let minLat = 90
+  let maxLat = -90
+  let minLong = 180
+  let maxLong = -180
   list.forEach((item) => {
     if (item.latitude < minLat) { minLat = item.latitude }
     if (item.latitude > maxLat) { maxLat = item.latitude }
     if (item.longitude < minLong) { minLong = item.longitude }
     if (item.longitude > maxLong) { maxLong = item.longitude }
   })
-  return [minLat, maxLat, minLong, maxLong];
+  return [minLat, maxLat, minLong, maxLong]
 }
 
 export const b64EncodeUnicode = (str) => {
-  return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g,
-  // this does not need to be named?
-    function toSolidBytes(match, p1) {
-      return String.fromCharCode('0x' + p1);
-    })
-  );
+  return btoa(
+    encodeURIComponent(str)
+    .replace(
+      /%([0-9A-F]{2})/g,
+      (match, p1) => { return String.fromCharCode('0x' + p1) }
+    )
+  )
 }
 
 export const b64DecodeUnicode = (str) => {
-  return decodeURIComponent(atob(str).split('').map((c) => {
-    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-  }).join(''));
+  return decodeURIComponent(
+    atob(str)
+    .split('')
+    .map(c => { return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2) })
+    .join(''))
 }
