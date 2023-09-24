f062dc1c660bedb95c8b26d839aaa2010c3d9175
ash <ash@ashs-MacBook-Pro.local>
2023-09-24 15:45:01 -0700

    vite json for deployment

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module CommandSearchExample
     config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
+    config.active_record.sqlite3_production_warning = false
 
     #
     # These settings can be overridden in specific environments using the files
